a7dc1c7e1bfd2dac1fed8f3ebe0c8695799a4632
Wimski <wimreckman@gmail.com>
2020-08-09 19:21:00 +0200

    Add resource type enum test

---
+++ tests/Enums/ResourceTypeEnumTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Wimski\Beatport\Tests\Enums;
+
+use Wimski\Beatport\Enums\ResourceTypeEnum;
+use Wimski\Beatport\Tests\TestCase;
+
+class ResourceTypeEnumTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider resourceTypes
+     */
+    public function it_returns_a_plural_value(string $value, string $plural): void
+    {
+        $enum = new ResourceTypeEnum($value);
+
+        static::assertEquals($plural, $enum->getValuePlural());
+    }
+
+    public function resourceTypes(): array
+    {
+        return [
+            ['artist',  'artists'],
+            ['genre',   'genres'],
+            ['label',   'labels'],
+            ['release', 'releases'],
+            ['track',   'tracks'],
+        ];
+    }
+}
